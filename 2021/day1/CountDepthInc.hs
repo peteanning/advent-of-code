0655528a6d476761ca32ba8f1628136069de2edb
Pete Anning <{ID}+{username}@users.noreply.github.com>
2021-12-06 10:31:05 +0000

    Finished day 1

--- 2021/day1/CountDepthInc.hs
+++ 2021/day1/CountDepthInc.hs
@@ -1,9 +1,14 @@
 module CountDepthInc where
+getIncreases :: [Int] -> Int -> Int
+getIncreases [] _ = 0
+getIncreases [n] _ = 0
+getIncreases (n:ns) acc = if n < head (ns) then 
+                            getIncreases ns acc + 1 else 
+                            getIncreases ns acc
+
 
 getWords :: FilePath -> IO ()
 getWords path = do contents <- readFile path
                    let depths =  map read (lines contents) :: [Int]
-                   let foo = reverse (zip depths [0,0..] )
-                   let r = foldr  (\(accDepth, accIncCount) (currentDepth, incCount) -> if currentDepth > accDepth then (currentDepth, accIncCount + 1) else (currentDepth, accIncCount)) (0,0) foo
-                   print r
+                   print $ getIncreases depths 0
 
--- 2021/day1/CountDepthIncSpec.hs
+++ 2021/day1/CountDepthIncSpec.hs
@@ -1,8 +1,17 @@
-module 2021.day1.CountDepthIncSpec where
+module CountDepthIncSpec where
 import Test.Hspec
-import 20201.day1.
-
-import Test.Hspec
+import CountDepthInc
 
 main :: IO ()
+main = hspec $ do
+  describe "count depth increase" $ do
+    it "should be increase of 0 with an empty array" $
+     getIncreases [] 0 `shouldBe` 0
+    it "should be increase of 0 with one depth" $
+     getIncreases [100] 0 `shouldBe` 0
+    it "should be increase of 0 with no increase in depth" $
+     getIncreases [100,99,98,10,9,8,7,6] 0 `shouldBe` 0
+    it "should be increase of 5 with 5 increases" $
+     getIncreases [10,11,12,11,10,9,12,13,14] 0 `shouldBe` 5
+
 
