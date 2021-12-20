09aaabb59f90b1e423c6f28c2368b3f43d9a91f6
Pete Anning <{ID}+{username}@users.noreply.github.com>
2021-12-20 18:35:18 +0000

    scored the game

--- 2021/day4/Bingo.hs
+++ 2021/day4/Bingo.hs
@@ -1,10 +1,10 @@
 module Bingo where
-import Data.List (dropWhileEnd)
 
-type BingoCard = [[(Int, Bool)]]
 type SetupFile = String
-type Game = [Int]
-
+type Pos = Int
+type Game = [(Int, Pos)]
+type BingoCard = [[Int]]
+type ScoredCard = [[(Int, Pos)]]
 
 split :: Char -> String -> [String]
 split d "" = []
@@ -14,25 +14,45 @@ split d xs = x : split d (drop 1 y)
 trim :: String -> String
 trim s = f (f s) 
       where f = reverse . dropWhile (== ' ')
-            
-game :: String -> [Int]
-game "" = []
-game s = map read (split  ',' s) :: [Int]
 
-card :: [String] -> [[[Int]]]
+game :: String -> [(Int,Pos)]
+game "" = []
+game s = zip (map read (split  ',' s) :: [Int]) [1..]
+
+
+card :: [String] -> BingoCards
 card (b:r1:r2:r3:r4:r5:rs) = (map ((map read . filter (/= "") . split ' ')) [r1, r2, r3, r4, r5] :: [[Int]])  : card rs
 card _ = []
+
+play :: String -> Int
+play gameFile = let x = lines gameFile :: [String]
+                    (gameRow,cards) = (head x, tail x) 
+                    g = game gameRow
+                    cs =  card  cards
+                in  0
+
+type BingoCards = [[[Int]]]
+scoreGame :: BingoCards -> Game -> [ScoredCard]
+scoreGame cs g = map (`score` g)  cs
+
+
+
+score :: BingoCard -> Game -> ScoredCard
+score c g = map (map (\n -> checkNumber n g)) c
+
+checkNumber :: Int -> Game -> (Int, Int)
+checkNumber n [] = (n,0) -- base case the number was not found in the game
+checkNumber n ((x,p):xs) = if n == x then (n, p) else checkNumber n xs
+
+
+
+
 
 
 loadsetup :: FilePath -> IO String
 loadsetup file = do 
                    contents <- readFile file
-                   let x = lines contents :: [String]
-                   let (gameRow,cards) = (head x, tail x) 
-                   let g = game gameRow
-                   let cs =  card  cards
-                   --print g
-                   --print cards
-                   print cs
+                   let result = play contents
+                   print result
                    return contents -- for now
               
--- 2021/day4/BingoSpec.hs
+++ 2021/day4/BingoSpec.hs
@@ -8,10 +8,15 @@ testFile :: String
 testFile = "7,4,9,5,11,17,23\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n" 
 testgame = "7,4,9,5,11,17,23"
 testgameStrArray = ["7","4","9","5","11","17","23"]
+testgameArray = [(22,1), (13,2), (17,3), (11,4), (0,5)]
 cards = ["", "22 13 17 11  0"," 8  2 23  4 24","21  9 14 16  7"," 6 10  3 18  5"," 1 12 20 15 19",""]
-testgameInts = [7,4,9,5,11,17,23] :: [Int]
+testgameInts = [(7,1),(4,2),(9,3),(5,4),(11,5),(17,6),(23,7)] :: [(Int,Int)]
 parsedCards :: [[[Int]]]
 parsedCards =[ [ [22, 13, 17, 11,  0],[ 8,  2, 23,  4, 24],[21,  9, 14, 16,  7],[ 6, 10,  3, 18,  5],[ 1, 12, 20, 15, 19] ]]
+parsedCard = [ [22, 13, 17, 11,  0],[ 8,  2, 23,  4, 24],[21,  9, 14, 16,  7],[ 6, 10,  3, 18,  5],[ 1, 12, 20, 15, 19] ]
+scoredCards :: [[[(Int,Int)]]]
+scoredCards =[ [ [(22,1), (13,2), (17,3), (11,4),  (0,5)],[ (8,0),  (2,0), (23,0),  (4,0), (24,0)],[(21,0),  (9,0), (14,0), (16,0),  (7,0)],[ (6,0), (10,0),  (3,0), (18,0),  (5,0)],[ (1,0), (12,0), (20,0), (15,0), (19,0)] ]]
+scoredCard = [ [(22,1), (13,2), (17,3), (11,4),  (0,5)],[ (8,0),  (2,0), (23,0),  (4,0), (24,0)],[(21,0),  (9,0), (14,0), (16,0),  (7,0)],[ (6,0), (10,0),  (3,0), (18,0),  (5,0)],[ (1,0), (12,0), (20,0), (15,0), (19,0)] ]
 
 
 main :: IO ()
@@ -31,7 +36,16 @@ main = hspec $ do
       trim " 0 " `shouldBe` "0"
     it "should parse cards" $ do
       card cards `shouldBe` parsedCards
-
+    it "Play a Game and get a scored result" $ do
+      play testFile `shouldBe` 0
+    it "convert a number to a scored number when it is found in the list" $ do
+      checkNumber 17 (zip [1..20] [1..]) `shouldBe` (17,17) 
+      checkNumber 21 (zip [1..20] [1..]) `shouldBe` (21, 0) 
+      checkNumber 28 [(28,1), (10,2)] `shouldBe` (28, 1) 
+    it "scores a game by matching the marked number and its called order" $ do
+      score parsedCard testgameArray `shouldBe` scoredCard      
+    it "scores multiple game cards" $ do
+      scoreGame parsedCards testgameArray `shouldBe` scoredCards
 
 
 
