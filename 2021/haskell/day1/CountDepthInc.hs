ad60165c7aa9d0d20eed14e439fc13923f8ae9cf
Pete Anning <{ID}+{username}@users.noreply.github.com>
2021-12-23 13:32:51 +0000

    added haskell

---
+++ 2021/haskell/day1/CountDepthInc.hs
@@ -0,0 +1,15 @@
+module CountDepthInc where
+
+getIncreases :: [Int] -> Int -> Int
+getIncreases [] _ = 0
+getIncreases [n] _ = 0
+getIncreases (n:ns) acc = if n < head (ns) then 
+                            getIncreases ns acc + 1 else 
+                            getIncreases ns acc
+
+
+getWords :: FilePath -> IO ()
+getWords path = do contents <- readFile path
+                   let depths =  map read (lines contents) :: [Int]
+                   print $ getIncreases depths 0
+
