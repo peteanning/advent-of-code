8b43c0d1b5adca79830950cdc851d3ad42ff7c93
Pete Anning <{ID}+{username}@users.noreply.github.com>
2021-12-24 18:02:24 +0000

    day5 complete

--- 2021/haskell/day5/HydrothermalVenture.hs
+++ 2021/haskell/day5/HydrothermalVenture.hs
@@ -1,19 +1,60 @@
 module HydrothermalVenture where
 
+import Data.List (delete, sort)
 import Split
 
+import qualified Data.Set as Set
+import Data.Set (empty, insert, notMember)
 
 type Line = (Pos,Pos)
 type Pos = (Int, Int)
+
+origin = (0,0) :: Pos
+zero = (origin, origin) :: Line
 
 parseLines :: String -> [Line]
 parseLines s = map (parseWords . words) $ lines s
 
 parseWords :: [String] -> Line
-parseWords [a, "->", b] = (parseTuple a, parseTuple b)
+parseWords [a, "->", b] = orderLine (parseTuple a, parseTuple b)
+parseWords _ = zero
+
+orderLine :: Line -> Line
+orderLine (p1, p2) = if p1 > p2 then (p2, p1) else (p1, p2)
 
 parseTuple :: String -> Pos
 parseTuple v = mkTuple $ split ',' v
 
 mkTuple :: [String] -> Pos
 mkTuple [x,y] = (read x :: Int , read y :: Int)
+mkTuple _ = origin
+
+linesCover :: Line -> [Pos]
+linesCover ((x0,y0), (x1, y1)) =
+     linesCover' x0 y0
+     where linesCover' x y | x0 == x1 && y <= y1 = (x, y) : linesCover' x (y + 1) -- x0 == x1 verical line
+                           | y0 == y1 && x <= x1 = (x, y) : linesCover' (x +1) y -- x0 < x1 horizontal line
+                           | otherwise = []
+
+pointCount :: [Pos] -> Int
+pointCount ps = pointCount' 0 empty (sort ps)
+                where pointCount' acc visited (p:ps) | not (null ps) =
+                          if p == head ps && notMember p visited then
+                             pointCount' (acc + 1) (insert p visited) ps else pointCount' acc visited ps
+                                             | otherwise = acc
+                      pointCount' acc _ [] = acc
+
+calcVentHotSpots :: String -> Int
+calcVentHotSpots filecontents = let i = parseLines filecontents
+                                    covers = concatMap linesCover i
+                                in
+                                   pointCount covers
+
+run :: String -> IO Int
+run f = do
+         contents <- readFile f
+         --print contents
+         return $ calcVentHotSpots contents
+
+
+
--- 2021/haskell/day5/HydrothermalVentureSpec.hs
+++ 2021/haskell/day5/HydrothermalVentureSpec.hs
@@ -1,7 +1,13 @@
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 module HydrothermalVentureSpec where
 
 import Test.Hspec
 import HydrothermalVenture
+
+line1 = ((0,0), (9,9)) :: Line
+line2 = ((9,9), (0,0))
+line3 = ((0,5), (0,1))
+
 
 main :: IO ()
 main = hspec $ do
@@ -11,7 +17,18 @@ main = hspec $ do
     it "should parse a String to a Tuple \"1,2\" to (1,2) " $ do
       parseTuple "1,2" `shouldBe` (1,2)   
     it "should parse a [String] of [\"0,9\", \"->\", \"1,2\"] to ((0,9), (1,2))" $ do
-      parseWords ["0,9", "->", "1,2"] `shouldBe` ((0,9), (1,2))   
+      parseWords ["0,0", "->", "9,9"] `shouldBe`  line1
+    it "should convert a Line to an array [Pos]" $ do
+      linesCover ((0,0), (0,3)) `shouldBe` [(0,0), (0,1), (0,2), (0,3)] 
+      --linesCover ((0,0), (0,0)) `shouldBe` [] 
+      linesCover ((0,0), (3,0)) `shouldBe` [(0,0), (1,0), (2,0), (3,0)]
+    it "should count the number of covered points" $ do
+      pointCount [(0,0), (0,1), (0,2), (0,3), (0,0), (0,1)]  `shouldBe` 2  
+      pointCount [(0,0), (0,1), (0,1), (0,3), (0,0), (0,1)]  `shouldBe` 2  
+    it "should order the positons in a line" $ do
+      orderLine line1 `shouldBe` line1 
+      orderLine line2 `shouldBe` line1
+      orderLine line3 `shouldBe` ((0,1), (0,5))
 
 
 
