c4180d337667def6bcb4041219f34f887c0eb568
Pete Anning <{ID}+{username}@users.noreply.github.com>
2021-12-29 21:12:17 +0000

    Day6 p1 & p2

--- 2021/haskell/day6/LanternFish.hs
+++ 2021/haskell/day6/LanternFish.hs
@@ -1,14 +1,18 @@
 module LanternFish where
 import Split ( split )
 import Data.List ( group, sort )
+import Data.Maybe ( fromMaybe )
 import qualified Data.Map as Map
+import System.Posix (BaudRate(B4800))
 
 -- group . sort . map (\x -> read x :: Int) .  concatMap (split ',') .  lines  <$> readFile "test.txt"
 
-loadAndRun :: String -> Int
-loadAndRun fileContents = let inputNumbers = fishFromStrArray fileContents
+loadAndRun :: String -> Int -> Int
+loadAndRun fileContents  days = let inputNumbers = fishFromStrArray fileContents
+                                    population = fishPopulation inputNumbers
+                                    grown = growPopulationByDays days population
                           in
-                            0
+                            countPopulation (Map.toList grown)
 
 fishFromStrArray :: String -> [[Int]]
 fishFromStrArray x = group . sort . map (\y -> read y :: Int) $ concatMap (split ',') (lines x)
@@ -21,6 +25,12 @@ balanceFish l h fish | l <= h = case Map.lookup l fish of Just f -> balanceFish 
                                                           Nothing -> balanceFish (l + 1) h (Map.insert l 0 fish)
                      | otherwise = fish
 
+countPopulation :: [(Int, Int)] -> Int 
+countPopulation [] = 0 
+countPopulation ((f,q):fs) = q + countPopulation fs
+
+
+
 growPopulationByDays :: Int -> Map.Map Int Int -> Map.Map Int Int
 growPopulationByDays n population | n > 0 = growPopulationByDays (n - 1) (growPopulation population)
                                   | otherwise = population
@@ -28,15 +38,13 @@ growPopulationByDays n population | n > 0 = growPopulationByDays (n - 1) (growPo
 
 growPopulation :: Map.Map Int Int -> Map.Map Int Int
 growPopulation fish = let agedPopulation =  balanceFish 0 8 $ Map.fromList (map decrementTimer onlyFishWithTimer)
-                          onlyFishWithTimer = filter (\(n,q) -> q /= 0) (Map.toList fish)
-                          spawning = agedPopulation Map.! 6
+                          onlyFishWithTimer = sort $ filter (\(n,q) -> q /= 0) (Map.toList fish)
                       in
-                         Map.insert 8 spawning agedPopulation
+                        spawn agedPopulation
+
 
 decrementTimer :: (Int, Int) -> (Int, Int)
-decrementTimer (n, q) | n == 0 = (6, q)
-                      | q > 0 = ((n -1), q)
-                      | otherwise = (n,q)
+decrementTimer (n, q)  = (n -1, q)
 
 toQuantiy :: [Int] -> (Int, Int)
 toQuantiy [x] = (x, 1)
@@ -44,10 +52,22 @@ toQuantiy [] = (-1, 0)
 toQuantiy (x:xs) = (x, length (x : xs))
 
 
-run :: String -> IO Int
-run path = do
+newBorn :: Map.Map Int Int -> Int 
+newBorn fish = fromMaybe 0 (Map.lookup (-1) fish) 
+
+spawn :: Map.Map Int Int -> Map.Map Int Int
+spawn fish = let nb =  newBorn fish
+                 cleanFish = Map.delete (-1) fish 
+                 sixes = (fromMaybe 0 (Map.lookup 6 cleanFish)) + nb
+             in
+               -- we should never get fish with a timer of 8
+               Map.insert 8 nb (Map.adjust (const sixes) 6 cleanFish)
+
+
+run :: String -> Int -> IO Int
+run path days = do
              contents <- readFile path
-             let result = loadAndRun contents
+             let result = loadAndRun contents days
              return result
 
 
--- 2021/haskell/day6/LanternFishSpec.hs
+++ 2021/haskell/day6/LanternFishSpec.hs
@@ -15,6 +15,7 @@ day1Fish = [[0],[1], [2,2],[3]] :: [[Int]]
 day2Fish = [[0],[1], [2,2],[3]] :: [[Int]]
 population = Map.fromList [(0,0),(1,1),(2,1), (3,2),(4,1),(5,0),(6,0),(7,0),(8,0)]
 populationAt1days = Map.fromList [(0,1), (1,1), (2,2), (3,1), (4,0), (5,0), (6,0), (7,0), (8,0)]
+populationAt4days = Map.fromList [(0,1), (1,0), (2,0), (3,0), (4,1), (5,1), (6,3), (7,1), (8,2)]
 extinct = Map.fromList [(0,0),(1,0),(2,0), (3,0)]
 
 main = hspec $ do
@@ -27,5 +28,23 @@ main = hspec $ do
       fishPopulation processedFish `shouldBe` population
     it "should grow the population" $ do  
       growPopulation population  `shouldBe` populationAt1days 
-      
- 
+    it "should grow the population by a given number of days" $ do
+      growPopulationByDays 4 population `shouldBe` populationAt4days 
+    it "should dcrement the timer correctly" $ do
+          decrementTimer (0, 1) `shouldBe` (-1,1)
+          decrementTimer (1, 1) `shouldBe` (0,1)
+          decrementTimer (1, 4) `shouldBe` (0,4) -- need more cases with quantity > 1
+          decrementTimer (2, 1) `shouldBe` (1,1)
+          decrementTimer (3, 1) `shouldBe` (2,1)
+          decrementTimer (4, 1) `shouldBe` (3,1)
+          decrementTimer (5, 1) `shouldBe` (4,1)
+          decrementTimer (6, 1) `shouldBe` (5,1)
+          decrementTimer (7, 1) `shouldBe` (6,1)
+          decrementTimer (8, 1) `shouldBe` (7,1)
+    it "should return the number of -1s aka newborn fish" $ do
+          newBorn (Map.fromList [(-1,1)]) `shouldBe` 1
+    it "should count the population" $ do
+          countPopulation (Map.toList  populationAt4days) `shouldBe` 9
+          countPopulation (Map.toList (growPopulationByDays 80 population)) `shouldBe` 5934
+
+
