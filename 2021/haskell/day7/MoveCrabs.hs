fb64ab7c19c37e9a24dbbd3990084f3c0dfbee80
Pete Anning <{ID}+{username}@users.noreply.github.com>
2022-01-03 15:43:55 +0000

    day7 complete

--- 2021/haskell/day7/MoveCrabs.hs
+++ 2021/haskell/day7/MoveCrabs.hs
@@ -1,5 +1,40 @@
 module MoveCrabs where
 
 import Data.List.Split
+import Data.List (sort, group)
 
+type Hpos = Int
+-- Data.List.group . Data.List.sort . concat . map (splitOn ",") . lines <$> readFile "test.txt"
+-- Data.List.sort . map (\x -> read x :: Int) . concat . map (splitOn ",") . lines <$> readFile "test.txt"
+sortedPositions :: String -> [Hpos]
+sortedPositions "" = []
+sortedPositions s = sort . map (\x -> read x :: Hpos) . concatMap  (splitOn ",") $ lines s
 
+frequency :: [Int] -> [(Int, Int)]
+frequency n =  let grouped = group n
+               in map (\(x:xs) -> (x, length (x:xs))) grouped
+
+cheapestMove :: [(Int, Int)] -> Int
+cheapestMove fs = findCheapest $ calculateMoveCosts (rawPositions fs) fs
+
+calculateMoveCosts (p:ps) fs = (p, totalCostOfMove (costOfMove p  fs)) : calculateMoveCosts ps fs
+calculateMoveCosts [] _ = []
+
+costOfMove :: Int -> [(Int, Int)] -> [(Int, Int)]
+costOfMove m = map (\(x,y) -> (x, y * abs (x - m)))
+
+totalCostOfMove :: [(Int, Int)] -> Int
+totalCostOfMove costs = sum $ map snd costs
+
+findCheapest :: [(Int, Int)] -> Int
+findCheapest cs = minimum $ map snd cs
+
+rawPositions :: [(Int, Int)] -> [Hpos]
+rawPositions fs = let positions = map fst fs
+                  in [(minimum positions)..(maximum positions)]
+
+run :: String -> IO Int 
+run filePath = do
+                  contents <- readFile filePath
+                  let result = cheapestMove . frequency $ sortedPositions contents
+                  return result 
--- 2021/haskell/day7/MoveCrabsSpec.hs
+++ 2021/haskell/day7/MoveCrabsSpec.hs
@@ -1,9 +1,33 @@
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 module Main where
 
 import Test.Hspec
+import MoveCrabs
+
+testFileContents = "16,1,2,0,4,2,7,1,2,14\n"
+positions = [0,1,1,2,2,2,4,7,14,16]
+frequencyMap = [(0,1), (1,2), (2,3), (4,1), (7,1), (14,1), (16,1)]
+costOfMovingZero = [(0,0), (1,2), (2,6), (4,4), (7,7), (14,14), (16,16)]
+costOfMovingOne = [(0,1), (1,0), (2,3), (4,3), (7,6), (14,13), (16,15)]
+uniquePostitions = [0..16]
+
 
 main = hspec $ do
-  describe "Vanilla Test" $ do
-    it "Foo should be true" $ do
-      1 == 1 `shouldBe` True
-
+  describe "MoveCrabs tests" $ do
+    it "should create a sorted list of positions from the test file loaded as a String" $ do
+      sortedPositions testFileContents `shouldBe` positions
+    it "should create a frequency  list" $ do
+      frequency positions `shouldBe` frequencyMap
+    it "should create a sorted list of unique postitions" $ do
+      rawPositions frequencyMap `shouldBe` [0..16]
+    it "should find the cost of a single move" $ do
+      costOfMove 0 frequencyMap `shouldBe` costOfMovingZero
+      costOfMove 1 frequencyMap `shouldBe` costOfMovingOne
+    it "should find the total cost of a move" $ do
+      totalCostOfMove costOfMovingZero `shouldBe` 49
+      totalCostOfMove costOfMovingOne `shouldBe` 41
+    it "should calculate the total of all the move costs " $ do
+      calculateMoveCosts uniquePostitions frequencyMap `shouldBe` [(0,49),(1,41),(2,37),(3,39),(4,41),(5,45),(6,49),(7,53),(8,59),(9,65),(10,71),(11,77),(12,83),(13,89),(14,95),(15,103),(16,111)]
+    it "should calculate the lowest cost move to align to the same postition" $ do
+      cheapestMove frequencyMap `shouldBe` 37 
+ 
