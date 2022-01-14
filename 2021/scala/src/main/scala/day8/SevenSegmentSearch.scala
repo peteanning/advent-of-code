2497e15eca547af9677a05499bd43942ccb510a3
Pete Anning <{ID}+{username}@users.noreply.github.com>
2022-01-14 18:08:34 +0000

    inital day8 part 2 hack

--- 2021/scala/src/main/scala/day8/SevenSegmentSearch.scala
+++ 2021/scala/src/main/scala/day8/SevenSegmentSearch.scala
@@ -7,23 +7,29 @@ object SevenSegmentSearch extends App:
   val D7_LEN = 3
   val D8_LEN = 7
 
+  val segmentsLengths = Set(D1_LEN, D4_LEN, D7_LEN, D8_LEN)
+  val trueMap = Map((D1_LEN, 1), (D4_LEN, 4), (D7_LEN, 7), (D8_LEN, 8))
+  def lookup (s: String) = trueMap.get(s.length)
+
   def loadData(fileName: String): List[String] =
     val source = io.Source.fromInputStream(getClass.getResourceAsStream(fileName))
     val text = try source.getLines.toList finally source.close()
     text
 
-  def splitSignalsAndObservations(rawData: List[String]): List[(String, List[String])] =
+  def spaceDelimitedToList(delimited: String):List[String] =
+    delimited.split(' ').toList
+
+  def splitSignalsAndObservations(rawData: List[String]): List[(List[String], List[String])] =
+    
     rawData.map {
         s => 
             val splitLine = s.split('|')
-            (splitLine(0).trim, splitLine(1).trim.split(' ').toList)
+            val digits = spaceDelimitedToList(splitLine(1).trim)
+            val signals = spaceDelimitedToList(splitLine(0).trim)
+            (signals, digits)
       }
 
-  def countUniqueDigits(parsedData: List[(String, List[String])]) =
-    val segmentsLengths = Set(D1_LEN, D4_LEN, D7_LEN, D8_LEN)
-    val trueMap = Map((D1_LEN, 1), (D4_LEN, 4), (D7_LEN, 7), (D8_LEN, 8))
-
-
+  def countUniqueDigits(parsedData: List[(List[String], List[String])]) =
     def _countUniqueDigits(observed: List[String]) = {
       observed.foldLeft(0){
         (acc,s) => if segmentsLengths.contains(s.length) then
@@ -33,6 +39,58 @@ object SevenSegmentSearch extends App:
                      acc
       }
     }
-
     parsedData.foldLeft(0)((acc, x) =>  acc + _countUniqueDigits(x._2))
+
+  def convertSignalsToKnownDigits(signals: List[String]): Map[String, Option[Int]] =
+      val s = signals.foldLeft(Map.empty[String, Option[Int]]){
+        (acc, value) => 
+              val maybeNumber = lookup(value)
+              maybeNumber match {
+                case n@Some(x) => acc ++ Map(value -> Some(x))
+                case _ => acc ++ Map(value -> None)
+        }
+      }
+      println(s)
+      s
+
+    // the digit 3 has 5 segments and overlaps the digit 7 in 3 of those
+    // the digit 2 has 5 segments and overlaps the digit 4 in 2 of those
+    // the digit 5 has 5 segments and overlaps the digit 4 in 3 of those
+    // the digit 9 has 6 segments and overlaps the digit 3 in 5 of those
+    // the digit 0 has 6 segments and overlaps the digit 1 in 2 of those
+    // the digit 6 is left over
+  def findOverlaps(knowns: Map[String, Option[Int]]): Map[String, Int] =
+    def getKnown(n: Int) =
+      knowns.find(_._2 == Some(n)).map(_._1).get
+ 
+        
+    val seven: String = getKnown(7)
+    val one: String = getKnown(1)
+    val four: String = getKnown(4)
+    val eight: String = getKnown(8)
+
+    val unknowns = knowns.filter(_._2 == None)
+    
+    def mapKnown (values: Map[String,Option[Int]], overlapWith: String, by: Int): String =
+       val r = values.find{
+           (k, v) => (k intersect overlapWith).length == by
+       }
+       r.get._1
+
+
+    val three: String = mapKnown(unknowns, seven, 3)
+    val two: String  =  mapKnown(unknowns.filterNot(_._1 == three), four, 2)
+    val five: String = mapKnown(unknowns, four, 3)
+    val nine: String = mapKnown(unknowns, three, 5)
+    val zero: String = mapKnown(unknowns, one, 2)
+    val six: String = knowns.filter{ 
+             (k, v) => List(one, two, three, four, five, seven, eight, nine).contains(k)
+        }.head._1
+
+    Map(zero -> 0, one -> 1, two -> 2, three -> 3, four -> 4, five -> 5, six -> 6, seven -> 7, eight -> 8, nine -> 9)
+
+
+
+
+
 
--- 2021/scala/src/test/scala/day8/SevenSegmentSearchSpec.scala
+++ 2021/scala/src/test/scala/day8/SevenSegmentSearchSpec.scala
@@ -11,21 +11,58 @@ class SevenSegmentSearchSpec extends org.scalatest.funsuite.AnyFunSuite {
   
   def parsedData = splitSignalsAndObservations(contents)
 
+  def s = splitSignalsAndObservations(List("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"))
+
+  def knowns = convertSignalsToKnownDigits(s(0)._1)
+
+
   test("SevenSegmentSearch loadData") {
     assert(contents.length == 10)
   }
+
   test("SevenSegmentSearch splitSignalsAndObservations") {
     assert(parsedData.length == 10)
   }
+
   test("should pare the whole line correctly") {
-    val s = splitSignalsAndObservations(List("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"))
+
+    assert(s(0)._1.length == 10)
+    assert(s(0)._1(0) == "acedgfb")
+    assert(s(0)._1(9) == "ab")
+
     assert(s(0)._2.length == 4)
     assert(s(0)._2(0) == "cdfeb")
-    assert(s(0)._2(1) == "fcadb")
-    assert(s(0)._2(2) == "cdfeb")
     assert(s(0)._2(3) == "cdbaf")
   }
+
   test("counting the unique digits in the obersved data") {
     assert(countUniqueDigits(parsedData) == 26)   
   }
+
+  test("should split a space delimited string into a list") {
+    val list = spaceDelimitedToList("this should split into a list")
+    assert(list.length == 6)   
+    assert(list(0) == "this")
+    assert(list(1) == "should")
+    assert(list(2) == "split")
+    assert(list(3) == "into")
+    assert(list(4) == "a")
+    assert(list(5) == "list")
+  }
+
+  test("should convert strings representing 1, 4, 7, 8") {
+
+    assert(knowns.get("ab").get == Some(1))
+    assert(knowns.get("dab").get == Some(7))
+    assert(knowns.get("eafb").get == Some(4))
+    assert(knowns.get("acedgfb").get == Some(8))
+
+  }
+
+  test("should find the overlaps between known and unknowns and map them to a full set of Digits") {
+    val all = findOverlaps(knowns)
+    assert(all.size == 10) 
+
+  }
+
 }
