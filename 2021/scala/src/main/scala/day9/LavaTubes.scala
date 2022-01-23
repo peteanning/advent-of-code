c8722ba17f36d77f820c9f79aa864d8492b33aad
Pete Anning <{ID}+{username}@users.noreply.github.com>
2022-01-23 14:41:42 +0000

    day9 part1

--- 2021/scala/src/main/scala/day9/LavaTubes.scala
+++ 2021/scala/src/main/scala/day9/LavaTubes.scala
@@ -2,12 +2,39 @@ package day9
 
 object LavaTubes extends App:
 
-  def walkMap(m: List[List[Int]]) =
-    for {
+  def walkMap(m: List[List[Int]]): List[(Int, Int, Int)] =
+    {for {
       i <- 0 until m.length
       j <- 0 until m(i).length
       v = m(i)(j)
       if lowest(i, j, m)
-    } yield (i, j, v)
+    } yield (i, j, v)}.toList
 
-  def lowest(i: Int, j: Int, m: List[List[Int]]):Boolean = true
+  def lowest(i: Int, j: Int, m: List[List[Int]]):Boolean = 
+    def default(d: (Int, Int, Boolean)): Int =
+      if !d._3 then m(d._1)(d._2) else 10
+
+    val up: (Int, Int, Boolean) = (i - 1, j, (i - 1 < 0))
+    val down: (Int, Int, Boolean)= (i + 1, j, (i+1 > m.length -1))
+    val left: (Int, Int, Boolean)= (i, j - 1, (j-1 < 0))
+    val right: (Int, Int, Boolean) = (i, j + 1, (j+1 > m(i).length -1))
+
+    //println(s"Up $up Down $down Left $left Right $right")
+
+    val h = m(i)(j)
+    val upHeight = default(up)
+    val downHeight = default(down)
+    val leftHeigh = default(left)
+    val rightHeigh = default(right)
+
+    h < upHeight && h < downHeight && h < leftHeigh && h < rightHeigh
+
+  def riskLevels(heights: List[(Int, Int, Int)]): Int =
+    heights.map(_._3 + 1).sum
+
+  def loadData(fileName: String): List[List[Int]] =
+    val source = io.Source.fromInputStream(getClass.getResourceAsStream(fileName))
+    val text = try source.getLines.toList finally source.close()
+    text.map(_.toArray.toList.map(_.asDigit))
+
+    
--- 2021/scala/src/test/scala/day9/LavaTubesSpec.scala
+++ 2021/scala/src/test/scala/day9/LavaTubesSpec.scala
@@ -1,11 +1,30 @@
 package day9
+import LavaTubes._
 
 class LavaTubesSpec extends org.scalatest.funsuite.AnyFunSuite {
 
-  def foo: String = "foo"
+  def m: List[List[Int]] = List(List(1,2,3), List(4,5,6), List(7,8,0))
 
-  test("Foo test") {
-    assert("bar" != foo)
+  def d = loadData("/day9/test.txt")
+
+  test("Walk the Map") {
+    assert(walkMap(m) == List((0,0,1), (2,2,0)))    
+  }
+
+  test("loadData") {
+    assert(d.length == 5)
+    assert(d(0).length == 10)
+    assert(d(0)(0) == 2)
+    assert(d(4)(9) == 8)
+
+  }
+
+  test("loadData and findLowest co-ordinates") {
+    assert(walkMap(d) == List((0,1,1), (0,9,0), (2,2,5), (4,6,5)))
+  }
+
+  test("The Sum of the heights plus one at the lowest points") {
+    assert(riskLevels(walkMap(d)) == 15)
   }
   
 }
