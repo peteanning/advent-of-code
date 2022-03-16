8e716c1d9e6b68005e3c8237d188bb0962d008ea
Peter Anning <22272449+peteanning@users.noreply.github.com>
2022-03-16 15:46:32 +0000

    skeleton working step

--- 2021/scala/src/main/scala/day11/DumboOctopus.scala
+++ 2021/scala/src/main/scala/day11/DumboOctopus.scala
@@ -2,43 +2,67 @@ package day11
 
 object DumboOctopus extends App:
 
+  type VisitedBy = Point
   def loadData(fileName: String): List[List[Int]] =
     val source = io.Source.fromInputStream(getClass.getResourceAsStream(fileName))
     val text = try source.getLines.toList finally source.close()
     text.map(_.toArray.toList.map(_.asDigit))
 
-  def makeOctopi(octopiMap: List[List[Int]]): Map[Pos, Octopus] = 
+  def makeOctopi(octopiMap: List[List[Int]]): Map[Point, Octopus] = 
     {for {
        y <- 0 until octopiMap.length
        x <- 0 until octopiMap(y).length
        v = octopiMap(x)(y)
-    } yield (Pos(x,y) -> Octopus(v))}.toMap
+    } yield (Point(x,y) -> Octopus(v))}.toMap
 
-  def increment(octopiMap: Map[Pos,Octopus]) =
-    octopiMap.mapValues(o => Octopus((o.v + 1))).toMap
+  def increment(octopiMap: Map[Point,Octopus]) =
+    octopiMap.mapValues(_.increment).toMap
     
-  def bounds(octopiMap: Map[Pos, Octopus]) =
+  def bounds(octopiMap: Map[Point, Octopus]) =
     octopiMap.keys.maxBy[(Int, Int)](p => (p.x, p.y))
 
+  def step(octopiMap: Map[Point, Octopus]) =
+    val b = bounds(octopiMap)
+    val incremented = increment(octopiMap)
+    val flashers = incremented.filter((_,o) => o.flashing).toList.map((p,o) => p)
+
+    def _step(flashingQueue: List[Point], os: Map[Point, Octopus], visited: Set[(VisitedBy, Point)]): Map[Point, Octopus] =
+      flashingQueue match {
+        case Nil => os
+        case p :: ps => {
+
+          val neighboursNotVisited : List[(VisitedBy, Point)] = 
+            (visited -- p.neighbours(b).map(point => (p,point)).toSet).toList
+          val neighboursInc = neighboursNotVisited.map((_,p) => (p, os(p).increment))
+          val updatedOctupi = os ++ neighboursInc.toMap 
+          val flashers = neighboursInc.filter((p,o) => o.flashing).map((p,_) => p)
+          val newVisited = visited ++ p.neighbours(b).map(point => (p, point))
+          _step(flashers ++ flashingQueue, updatedOctupi, newVisited)
+        }
+      }
+    
+    _step(flashers, incremented, Set.empty)
 
 case class Octopus(v: Int) {
   def flashing: Boolean = v > 9
+  def increment = new Octopus(v + 1)
 }
 
-case class Pos(x: Int, y: Int) {
-    def upLeft = Pos(x - 1, y + 1)
-    def up = Pos(x, y + 1)
-    def upRight =  Pos(x + 1, y + 1)
-    def left = Pos(x - 1, y)
-    def right = Pos(x + 1, y)
-    def downLeft = Pos(x - 1, y - 1) 
-    def down = Pos(x, y - 1)
-    def downRight = Pos(x + 1, y - 1)
-    def neighbours(bounds: Pos) = List(upLeft, up, upRight, left, right, downLeft, down, downRight)
+case class Point(x: Int, y: Int) {
+    def upLeft = Point(x - 1, y + 1)
+    def up = Point(x, y + 1)
+    def upRight =  Point(x + 1, y + 1)
+    def left = Point(x - 1, y)
+    def right = Point(x + 1, y)
+    def downLeft = Point(x - 1, y - 1) 
+    def down = Point(x, y - 1)
+    def downRight = Point(x + 1, y - 1)
+    def neighbours(bounds: Point) = List(upLeft, up, upRight, left, right, downLeft, down, downRight)
       .filter(p => (p.x >= 0 && p.y >= 0 && p.x <= bounds.x && p.y <= bounds.y)) 
 }
 
-object Pos:
-  // ordering converts a Pos into a tuple and uses the builtin ordering for that
-  given orderingByPos[A <: Pos]: Ordering[Pos] = Ordering.by (p => (p.x, p.y))
+object Point:
+  // ordering converts a Point into a tuple and uses the builtin ordering for that
+  given orderingByPoint[A <: Point]: Ordering[Point] = Ordering.by (p => (p.x, p.y))
+
 
--- 2021/scala/src/test/scala/day11/DumboOctopusSpec.scala
+++ 2021/scala/src/test/scala/day11/DumboOctopusSpec.scala
@@ -10,7 +10,7 @@ class DumboOctopusSpec extends org.scalatest.funsuite.AnyFunSuite {
 
   def smallData: List[List[Int]] = loadData("/day11/small-test.txt")
 
-  def ZERO: Pos = Pos(0,0)
+  def ZERO: Point = Point(0,0)
 
 
   test("shoudl load the test data") {
@@ -26,49 +26,49 @@ class DumboOctopusSpec extends org.scalatest.funsuite.AnyFunSuite {
     val octopi = makeOctopi(smallData)
     assert(octopi.size == 25)
     assert(octopi.get(ZERO).get == Octopus(1))
-    assert(octopi.get(Pos(4,4)).get == Octopus(1))
+    assert(octopi.get(Point(4,4)).get == Octopus(1))
   }
   test("increment") {
     val octopi = makeOctopi(smallData)
     val i = increment(octopi)
     assert(i.get(ZERO).get == Octopus(2))
-    assert(i.get(Pos(4,4)).get == Octopus(2))
+    assert(i.get(Point(4,4)).get == Octopus(2))
   }
   test("bounds") {
     val b = bounds(makeOctopi(smallData))
-    assert(b == Pos(4,4))
+    assert(b == Point(4,4))
   }
 
-  test("a  Pos at 0,0 only has 3 neighbours with bound 2,2") {
-   val neighbours: List[Pos] = ZERO.neighbours(Pos(2,2)) 
-   val expected: List[Pos] = List(Pos(0,1), Pos(1,0), Pos(1,1))
+  test("a  Point at 0,0 only has 3 neighbours with bound 2,2") {
+   val neighbours: List[Point] = ZERO.neighbours(Point(2,2)) 
+   val expected: List[Point] = List(Point(0,1), Point(1,0), Point(1,1))
    assert(neighbours.size == 3)
    assert(neighbours.sorted == expected.sorted)
   }
-  test("a  Pos at 2,2 only has 3 neighbours with bound  2,2") {
-   val neighbours: List[Pos] = Pos(2,2).neighbours(Pos(2,2)) 
-   val expected: List[Pos] = List(Pos(1,2), Pos(1,1), Pos(2,1))
-   assert(neighbours.size == 3)
-   assert(neighbours.sorted == expected.sorted)
-  }
-
-  test("a  Pos at 0,2 only has 3 neighbours with bound  2,2") {
-   val neighbours: List[Pos] = Pos(0,2).neighbours(Pos(2,2)) 
-   val expected: List[Pos] = List(Pos(1,2), Pos(1,1), Pos(0,1))
+  test("a  Point at 2,2 only has 3 neighbours with bound  2,2") {
+   val neighbours: List[Point] = Point(2,2).neighbours(Point(2,2)) 
+   val expected: List[Point] = List(Point(1,2), Point(1,1), Point(2,1))
    assert(neighbours.size == 3)
    assert(neighbours.sorted == expected.sorted)
   }
 
-  test("a  Pos at 2,0 only has 3 neighbours with bound  2,2") {
-   val neighbours: List[Pos] = Pos(2,0).neighbours(Pos(2,2)) 
-   val expected: List[Pos] = List(Pos(1,0), Pos(1,1), Pos(2,1))
+  test("a  Point at 0,2 only has 3 neighbours with bound  2,2") {
+   val neighbours: List[Point] = Point(0,2).neighbours(Point(2,2)) 
+   val expected: List[Point] = List(Point(1,2), Point(1,1), Point(0,1))
    assert(neighbours.size == 3)
    assert(neighbours.sorted == expected.sorted)
   }
-  test("a  Pos at 1,1  8 neighbours with bound  2,2") {
-   val neighbours: List[Pos] = Pos(1,1).neighbours(Pos(2,2)) 
-   val expected: List[Pos] = List(Pos(0,2), Pos(1,2), Pos(2,2), Pos(0,1), Pos(0,0), Pos(2,1),
-     Pos(1,0), Pos(2,0))
+
+  test("a  Point at 2,0 only has 3 neighbours with bound  2,2") {
+   val neighbours: List[Point] = Point(2,0).neighbours(Point(2,2)) 
+   val expected: List[Point] = List(Point(1,0), Point(1,1), Point(2,1))
+   assert(neighbours.size == 3)
+   assert(neighbours.sorted == expected.sorted)
+  }
+  test("a  Point at 1,1  8 neighbours with bound  2,2") {
+   val neighbours: List[Point] = Point(1,1).neighbours(Point(2,2)) 
+   val expected: List[Point] = List(Point(0,2), Point(1,2), Point(2,2), Point(0,1), Point(0,0), Point(2,1),
+     Point(1,0), Point(2,0))
    assert(neighbours.size == 8)
    assert(neighbours.sorted == expected.sorted)
   }
