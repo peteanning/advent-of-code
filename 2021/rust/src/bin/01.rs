210a37e3816ac6d057bf80b99d8e795a3502ba38
Peter Anning <22272449+peteanning@users.noreply.github.com>
2022-11-07 13:51:54 +0000

    Day 1 part 1 2021 in RUST

--- 2021/rust/src/bin/01.rs
+++ 2021/rust/src/bin/01.rs
@@ -1,10 +1,25 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use std::convert::TryFrom;
+
+pub fn part_one(input: &str) -> Option<usize> {
+
+    let result =
+            input
+            .lines()
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<u32>>()
+            .windows(2)
+            .flat_map(<&[u32; 2]>::try_from)
+            .filter(|&&[a,b]| a < b)
+            .count();
+    
+    Some(result)
+
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
 }
+
 
 fn main() {
     let input = &aoc::read_file("inputs", 1);
@@ -19,7 +34,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = aoc::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(7));
     }
 
     #[test]
--- 2021/rust/src/helpers.rs
+++ 2021/rust/src/helpers.rs
@@ -2,3 +2,18 @@
  * Use this file if you want to extract helpers from your solutions.
  * Example import from this file: `use aoc::helpers::example_fn;`.
  */
+
+fn an_experiment() -> usize  {
+    let bar: Vec<u64> = vec![2, 4, 3, 7]; // For example
+
+    let res = bar
+        .windows(2)
+        .flat_map(<&[u64; 2]>::try_from)
+        .filter(|&&[a,b]| a < b)
+        .count(); 
+
+
+    println!("The result was {}", res);
+
+    res
+}
