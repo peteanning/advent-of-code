8b029e5c036379f1f9ad17ba52ae63c21fba724b
Peter Anning <22272449+peteanning@users.noreply.github.com>
2022-11-07 14:48:48 +0000

    Day 1 part 2 2021 in RUST

--- 2021/rust/src/bin/01.rs
+++ 2021/rust/src/bin/01.rs
@@ -16,8 +16,22 @@ pub fn part_one(input: &str) -> Option<usize> {
 
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let count =
+            input
+            .lines()
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<u32>>()
+            .windows(3)
+            .flat_map(<&[u32; 3]>::try_from)
+            .map(|s| s.into_iter().sum())
+            .collect::<Vec<u32>>()
+            .windows(2)
+            .flat_map(<&[u32; 2]>::try_from)
+            .filter(|&&[a,b]| a < b)
+            .count();
+    
+    Some(count)
 }
 
 
@@ -33,13 +47,20 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = aoc::read_file("examples", 1);
+        let mut input = aoc::read_file("examples", 1);
         assert_eq!(part_one(&input), Some(7));
+
+        input = aoc::read_file("inputs", 1);
+        assert_eq!(part_one(&input), Some(1228))
     }
 
     #[test]
     fn test_part_two() {
-        let input = aoc::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        let mut input = aoc::read_file("examples", 1);
+        assert_eq!(part_two(&input), Some(5));
+
+        input = aoc::read_file("inputs", 1);
+        assert_eq!(part_two(&input), Some(1257))
+
     }
 }
