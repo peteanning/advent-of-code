b374082983a0243c5182f21d43d4424e28f63994
peter <22272449+peteanning@users.noreply.github.com>
2022-12-03 22:34:57 +0000

    Day 2 part 2

--- 2022/rust/src/bin/02.rs
+++ 2022/rust/src/bin/02.rs
@@ -20,14 +20,15 @@ const DRAW: u32 = 3;
 
 impl Round {
     
-    pub fn new(their_choice: &str, your_choice: &str) -> Result<Round, String> {
-        match (Round::str_to_choice(their_choice), Round::str_to_choice(your_choice))  {
+    pub fn new(their_choice: &str, your_choice: &str, eval: fn(&str, Option<&str>) -> Result<Choice, String>) -> Result<Round, String> {
+        match (Round::str_to_choice(their_choice, None), eval(their_choice, Some(your_choice)))  {
             (Ok(theirs), Ok(yours)) => Ok(Round { their_choice: theirs, your_choice: yours }),
             _ => Err(format!("Unkown choice their's {}, yours {}", their_choice, your_choice))
         }
     }
 
-    fn str_to_choice(choice: &str) -> Result<Choice, String> {
+
+    fn str_to_choice(choice: &str, _your_choice: Option<&str>) -> Result<Choice, String> {
         match choice {
             "A" => Ok(Choice::Rock),
             "B" => Ok(Choice::Paper),
@@ -39,7 +40,23 @@ impl Round {
         }
     }
 
-    
+    fn str_to_choice_with_outcome(choice: &str, your_choice: Option<&str>) -> Result<Choice, String> {
+        match (choice, your_choice) {
+            ("A", Some("X")) => Ok(Choice::Scissors), // loose against Rock
+            ("A", Some("Y")) => Ok(Choice::Rock), // draw
+            ("A", Some("Z")) => Ok(Choice::Paper), // Win
+
+            ("B", Some("X")) => Ok(Choice::Rock), //loose against Paper
+            ("B", Some("Y")) => Ok(Choice::Paper), //draw
+            ("B", Some("Z")) => Ok(Choice::Scissors), //win
+
+
+           ("C", Some("X")) => Ok(Choice::Paper), //loose aainst Scissors
+           ("C", Some("Y")) => Ok(Choice::Scissors), //draw
+           ("C", Some("Z")) => Ok(Choice::Rock), // win
+                _ => Err("Unknown Choice".to_string())
+        }
+    }
     fn score(round: Round) -> u32 {
         match round {
             Round { their_choice: Choice::Rock, your_choice: Choice::Rock } => DRAW + ROCK,
@@ -60,33 +77,48 @@ impl Round {
     
 }
 
+fn evalualte_rounds(input: Vec<Vec<&str>>, eval: fn(&str, Option<&str>) -> Result<Choice, String>) -> Vec<Round> {
+    input.into_iter()
+        .map(|v| {
+            match Round::new(v[0], v[1], eval) {
+                Ok(round) => round,
+                _ => panic!("unable to parse input")
+            }}).collect::<Vec<Round>>()
+}
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let result: Vec<Vec<&str>>  = 
+fn parse(input: &str) -> Vec<Vec<&str>> {
         input
         .lines()
         .into_iter()
         .map(|l| l.split(' ').collect::<Vec<&str>>())
-        .collect();
+        .collect()
+}
 
-    let rounds =
-        result.into_iter()
-        .map(|v| {
-            match Round::new(v[0],v[1]) {
-                Ok(round) => round,
-                _ => panic!("unable to parse input")
-            }}).collect::<Vec<Round>>();
+fn scores(rounds: Vec<Round>) -> Vec<u32> {
+  rounds.into_iter()
+        .map(|r| Round::score(r)).collect()
+}
 
-    let scores: Vec<u32> = 
-        rounds.into_iter()
-        .map(|r| Round::score(r)).collect();
+pub fn part_one(input: &str) -> Option<u32> {
+    let parsed: Vec<Vec<&str>>  = parse(input);
+
+    let rounds: Vec<Round> = evalualte_rounds(parsed, Round::str_to_choice);
+
+    let scores: Vec<u32> = scores(rounds);
 
     Some(scores.into_iter().sum())
 
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let parsed: Vec<Vec<&str>>  = parse(input);
+
+    let rounds: Vec<Round> = evalualte_rounds(parsed, Round::str_to_choice_with_outcome);
+
+    let scores: Vec<u32> = scores(rounds);
+
+    Some(scores.into_iter().sum())
+
 }
 
 fn main() {
@@ -108,6 +140,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
